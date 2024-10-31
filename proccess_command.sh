9f9066813f14f455c1fc741a65d0830e365e6f7d
SickCore69 <102438713+SickCore69@users.noreply.github.com>
2024-10-31 17:55:42 -0600

    Update proccess_command.sh

--- proccess_command.sh
+++ proccess_command.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function ctrl_c(){
-  echo -e "\n\n[!] Saliendo...\n"
-	tput cnorm; exit 1
+    echo -e "\n\n[!] Saliendo...\n"
+    tput cnorm; exit 1
 }
 
 # Ctrl+C
@@ -13,9 +13,9 @@ old_process=$(ps -eo user,command)
 tput civis # Ocultar el cursor
 
 while true; do
-	new_process=$(ps -eo user,command)
-	diff <(echo "$old_process") <(echo "$new_process") | grep "[\>\<]" | grep -vE "command|kworker|procmon"  #Aplicar una diferencia entre la variable old_process y new_process
-	old_process=$new_process    # Actualizar la variable old_process con el contenido de new_process para que la diferencia aplicada no sea infinita.
+    new_process=$(ps -eo user,command)
+    diff <(echo "$old_process") <(echo "$new_process") | grep "[\>\<]" | grep -vE "command|kworker|procmon"  #Aplicar una diferencia entre la variable old_process y new_process
+    old_process=$new_process    # Actualizar la variable old_process con el contenido de new_process para que la diferencia aplicada no sea infinita.
 done
 
 tput cnorm
